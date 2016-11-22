5d57fc9ab09538e9894a2f2daf2ba5426be04ab6
HowcanoeWang <howcanoewang@gmail.com>
2016-11-22 11:26:56 -0400

    Beta1.6.8

--- Workspace/iconvert.py
+++
@@ -1,9 +0,0 @@
-import base64
-open_icon = open("icon.png","rb")
-b64str = base64.b64encode(open_icon.read())
-print(b64str)
-open_icon.close()
-write_data = "img = " + str(b64str)
-f = open("icon.py","w+")
-f.write(write_data)
-f.close()
